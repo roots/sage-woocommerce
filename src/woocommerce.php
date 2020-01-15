6ce1d497a5f54a71eee6685852b87526f0902a5b
Patrick Vézina <patrick@croustille.io>
2020-01-15 16:27:57 -0500

    First draft for sage-10

--- src/woocommerce.php
+++ src/woocommerce.php
@@ -1,5 +1,5 @@
 <?php
-namespace App;
+namespace Roots;
 
 if (defined('WC_ABSPATH')) {
     add_action('after_setup_theme', function () {
@@ -12,53 +12,73 @@ if (defined('WC_ABSPATH')) {
      */
     function wc_template_loader(String $template)
     {
-        return strpos($template, WC_ABSPATH) === -1
-            ? $template
-            : (locate_template(WC()->template_path() . str_replace(WC_ABSPATH . 'templates/', '', $template)) ? : $template);
+        if (strpos($template, WC_ABSPATH) === -1) {
+            return $template;
+        }
+        $name = WC()->template_path() . str_replace(WC_ABSPATH . 'templates/', '', $template);
+        return locate_template(app('sage.finder')->locate($name)) ? : $template;
     }
-    add_filter('template_include', __NAMESPACE__ . '\\wc_template_loader', 100, 1);
+    add_filter('template_include', __NAMESPACE__ . '\\wc_template_loader', 90, 1);
     add_filter('comments_template', __NAMESPACE__ . '\\wc_template_loader', 100, 1);
 
     add_filter('wc_get_template_part', function ($template) {
-        $theme_template = locate_template(WC()->template_path() . str_replace(WC_ABSPATH . 'templates/', '', $template));
+        $name = WC()->template_path() . str_replace(WC_ABSPATH . 'templates/', '', $template);
+        $theme_template = locate_template(app('sage.finder')->locate($name));
 
         if ($theme_template) {
-            $data = collect(get_body_class())->reduce(function ($data, $class) {
-                return apply_filters("sage/template/{$class}/data", $data);
+            $view = app('view.finder')
+                ->getPossibleViewNameFromPath($file = realpath($theme_template));
+
+            $view = trim($view, '\\/.');
+
+            /** Gather data to be passed to view */
+            $data = array_reduce(get_body_class(), function ($data, $class) use ($view, $file) {
+                return apply_filters("sage/template/{$class}/data", $data, $view, $file);
             }, []);
 
-            echo template($theme_template, $data);
-            return get_stylesheet_directory() . '/index.php';
+            echo view($view, $data)->render();
         }
 
         return $template;
     }, PHP_INT_MAX, 1);
 
     add_action('woocommerce_before_template_part', function ($template_name, $template_path, $located, $args) {
-        $theme_template = locate_template(WC()->template_path() . $template_name);
+        $name = WC()->template_path() . $template_name;
+        $theme_template = locate_template(app('sage.finder')->locate($name));
 
         if ($theme_template) {
-            $data = collect(get_body_class())->reduce(function ($data, $class) {
-                return apply_filters("sage/template/{$class}/data", $data);
+            $view = app('view.finder')
+                ->getPossibleViewNameFromPath($file = realpath($theme_template));
+
+            $view = trim($view, '\\/.');
+
+            /** Gather data to be passed to view */
+            $data = array_reduce(get_body_class(), function ($data, $class) use ($view, $file) {
+                return apply_filters("sage/template/{$class}/data", $data, $view, $file);
             }, []);
 
-            echo template($theme_template, array_merge(
+            echo view($view, array_merge(
                 compact(explode(' ', 'template_name template_path located args')),
                 $data,
                 $args
-            ));
+            ))->render();
         }
     }, PHP_INT_MAX, 4);
 
     add_filter('wc_get_template', function ($template, $template_name, $args) {
-        $theme_template = locate_template(WC()->template_path() . $template_name);
+        $name = WC()->template_path() . $template_name;
+        $theme_template = locate_template(app('sage.finder')->locate($name));
 
         // return theme filename for status screen
-        if (is_admin() && ! wp_doing_ajax() && function_exists('get_current_screen') && get_current_screen() && get_current_screen()->id === 'woocommerce_page_wc-status') {
+        if (is_admin() &&
+            ! wp_doing_ajax() &&
+            function_exists('get_current_screen') &&
+            get_current_screen() &&
+            get_current_screen()->id === 'woocommerce_page_wc-status') {
             return $theme_template ? : $template;
         }
 
         // return empty file, output already rendered by 'woocommerce_before_template_part' hook
-        return $theme_template ? get_stylesheet_directory() . '/index.php' : $template;
+        return $theme_template ? get_template_directory() . '/index.php' : $template;
     }, 100, 3);
 }
